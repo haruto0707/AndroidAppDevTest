a615faef2847b162acfed2f5d1dc803c23eedad9
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-09-07 14:58:19 +0900

    モバイルアプリ開発授業4日目の内容

---
+++ app/src/main/java/jp/ac/meijou/android/s221205070/NetworkActivity.java
@@ -0,0 +1,70 @@
+package jp.ac.meijou.android.s221205070;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s221205070.databinding.ActivityNetworkBinding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class NetworkActivity extends AppCompatActivity {
+
+    private ActivityNetworkBinding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNetworkBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button16.setOnClickListener(view -> {
+            var text = binding.editTextText3.getText().toString();
+
+            var url = Uri.parse("https://placehold.jp/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+
+            getImage(url);
+        });
+    }
+
+    private void getImage(String url) {
+        // リクエスト先に画像URLを指定
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+        // 非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // 通信に失敗した時に呼ばれる
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // InputStreamをBitmapに変換
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+                runOnUiThread(() -> binding.imageView.setImageBitmap(bitmap));
+            }
+        });
+    }
+}
